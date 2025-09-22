82b1650a5bb243d7991106476aed9afe971e2be8
keyhoney <ip9mong@gmail.com>
2025-09-22 11:08:09 +0900

    이름행 가로 길이 조정

--- student.js
+++ student.js
@@ -35,7 +35,7 @@ async function renderStudent() {
         <label>번호</label>
         <select id="number"></select>
       </div>
-      <div style="flex:0 0 200px">
+      <div style="flex:0 0 250px">
         <label>이름</label>
         <input id="name" type="text" placeholder="반과 번호를 선택하면 자동으로 불러옵니다" />
       </div>
@@ -216,4 +216,5 @@ document.addEventListener('DOMContentLoaded', () => {
   
   renderStudent();
 });
+
 
